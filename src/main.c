e2b167c2e0a01d1e859d3bcd6cc8c8f5a972feb8
clumsyyyy <owencwijaya@gmail.com>
2021-10-22 13:39:12 +0700

    Memindahkan fungsi config ke file terpisah

--- src/main.c
+++ src/main.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-#include "ADT/Matrix/matrix.c"
-#include "ADT/MesinKata/wordmachine.c"
-#include "ADT/MesinKata/charmachine.c"
-
+#include "configInput.c"
 /* prototype fungsi */
 int mainMenu();
-void configInput();
 void title();
 
 /* Fungsi utama yang akan di-run */
@@ -35,19 +31,6 @@ int mainMenu(){
     }
 }
 
-void configInput(char fileName[100]){
-    FILE *file;
-    char temp[100];
-    printf("Nama file: %s\n\n", fileName);
-    file = fopen(".//config/dummyConfig.txt", "r"); // hardcoded dulu sementara, buat ngetes
-    if (file != NULL){ //pake algo mesin kata?
-        fscanf(file, "%s", temp);
-        printf("%s", temp);
-    } else {
-        printf("File tidak ditemukan. Kembali ke menu utama....\n");
-        mainMenu();
-    }
-}
 
 void title(){
     printf("  __                         \n");
