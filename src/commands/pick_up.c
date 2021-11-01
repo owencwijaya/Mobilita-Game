2faf26a03765880d8f63edd53cb2fcf3650ddd99
clumsyyyy <owencwijaya@gmail.com>
2021-11-01 12:22:06 +0700

    TODO: memperbaiki fungsi countItemInLocation

--- src/commands/pick_up.c
+++ src/commands/pick_up.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include "../modules/core/globals.h"
-#include "macros.h"
-
+#include "../models/macros.h"
+#include "../models/boolean.h"
 
 int idxVIPintodo(ItemList todo){
 /*Mengembalikan idx VIP item di To Do list, jika tidak ada mengembalikan -1
@@ -42,13 +42,14 @@ int countItemInLocation(ItemList todo){
     ItemList l;
     l = todo;
     int count = 0;
-    while(l != NULL){
+    for(int i = 0; i < itemListLength(todo); i++){
         if(isLocationIdentical(gameState.currentLocation, value(l).pickUpLocation)){
-            count +=1;
-        }else{
-            l = next(l);
+            count += 1;
+            printf("%d", count);
         }
+        l = next(l);
     }
+    return count;
 }
 
 void insertItem(ItemList *inProg, Item item){
@@ -90,8 +91,9 @@ void pick_up(){
             printf("Pesanan berupa VIP item tidak ditemukan\n\n");
         }
     }else{
-        if(gameState.currentLocation.isPickUpPlace){
+        if(gameState.gameMap._locationMatrix.contents[gameState.currentLocation.coordinate.x][gameState.currentLocation.coordinate.y].isPickUpPlace){
             int countItem = countItemInLocation(gameState.todoList);
+            printf("%d", countItem);
             int i;
             for(i = 0; i<countItem; i++){//Loop Untuk Mengambil Beberapa Item pada Pick Up Place
                 int idx = idxItemInTodo(gameState.todoList);
@@ -112,6 +114,12 @@ void pick_up(){
                         deleteItemAt(&gameState.todoList, idx, &temp);
                         gameState.abs.IsHeavyItemOn = true;
                         gameState.abs.HeavyItemStack++;
+                        if (gameState.abs.SpeedBoost){
+                            gameState.abs.SpeedBoost = false;
+                            gameState.abs.SpeedBoostCount = 0;
+                            gameState.abs.SpeedBoostStack = 0;
+                            printf("Speed Boost yang didapatkan dimatikan!");
+                        }
                     }else if(isPerishableItem(getItem(gameState.todoList, idx))){
                         insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
                         push(&gameState.bag, getItem(gameState.todoList, idx));
