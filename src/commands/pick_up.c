bdce7b6b7c064f467e2a21273ffeee51c870dfb3
afrizalsebastian <sebastiangurning@gmail.com>
2021-11-05 15:45:18 +0700

    fix pick_up

--- src/commands/pick_up.c
+++ src/commands/pick_up.c
@@ -3,22 +3,35 @@
 #include "../models/macros.h"
 #include "../models/boolean.h"
 
-int idxVIPintodo(ItemList todo){
-/*Mengembalikan idx VIP item di To Do list, jika tidak ada mengembalikan -1
-Jika idx =/= -1, maka VIP item ada di ToDoList*/
+boolean isVIPintodo(ItemList todo){
+/*Mengembalikan True Jika VIP item ada di TO_DO*/
     ItemList l;
     l = todo;
-    int idx = 0;
     boolean found = false;
     while (l != NULL && !found){
         if(isVIPItem(value(l))){
             found = true;
         }else{
-            idx ++;
             l = next(l);
         }
     }
-   return found ? idx : -1;
+    return found;
+}
+
+int idxVIPItem(ItemList todo, Location location){
+    ItemList l;
+    l = todo;
+    int idx = 0;
+    boolean found = false;
+    while (l != NULL && !found){
+        if(isVIPItem(value(l)) && isLocationIdentical(location, value(l).pickUpLocation)){
+            found = true;
+        }else{
+            idx +=1;
+            l = next(l);
+        }
+    }
+    return found ? idx : -1;
 }
 
 int idxItemInTodo(ItemList todo){
@@ -37,20 +50,6 @@ int idxItemInTodo(ItemList todo){
     return idx;
 }
 
-int countItemInLocation(ItemList todo){
-// Banyaknya Item yang dapat diambil di Pick Up Place. 
-    ItemList l;
-    l = todo;
-    int count = 0;
-    for(int i = 0; i < itemListLength(todo); i++){
-        if(isLocationIdentical(gameState.currentLocation, value(l).pickUpLocation)){
-            count += 1;
-            printf("%d", count);
-        }
-        l = next(l);
-    }
-    return count;
-}
 
 void insertItem(ItemList *inProg, Item item){
 // Menambahkan Item ke In Progress terurut dari pesanan yang terakhir kali masuk agar lebih mudah ditampilkan (command inProgress)
@@ -76,62 +75,60 @@ void insertItem(ItemList *inProg, Item item){
 }
 
 void pick_up(){
-    if(idxVIPintodo(gameState.todoList) != -1){
+    if(isVIPintodo(gameState.todoList)){
     //Ada VIP Item di To Do list, sehingga Harus diutamakan terlebih dahulu.
-        int idx = idxVIPintodo(gameState.todoList);
-        printf("%c", getItem(gameState.todoList, idx).pickUpLocation.symbol);
-        if(isLocationIdentical(gameState.currentLocation, getItem(gameState.todoList, idx).pickUpLocation)){
-            insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
-            push(&gameState.bag, getItem(gameState.todoList, idx));
-            printf("Pesanan berupa VIP Item berhasil diambil!\n");
-            printf("Tujuan Pesanan : %c\n\n", getItem(gameState.todoList, idx).dropOffLocation.symbol);
-            gameState.abs.IsVIPItemOn = true;
-            Item temp;
-            deleteItemAt(&gameState.todoList, idx, &temp); //Menghapus VIP item dari ToDo List
+        if(!isStackFull(gameState.bag)){
+            int idx = idxVIPItem(gameState.todoList, gameState.currentLocation);
+            if(idx != -1){
+                insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
+                push(&gameState.bag, getItem(gameState.todoList, idx));
+                printf("Pesanan berupa VIP Item berhasil diambil!\n");
+                printf("Tujuan Pesanan : %c\n\n", getItem(gameState.todoList, idx).dropOffLocation.symbol);
+                gameState.abs.IsVIPItemOn = true;
+                Item temp;
+                deleteItemAt(&gameState.todoList, idx, &temp); //Menghapus VIP item dari ToDo List
+            }else{
+                printf("Pesanan berupa VIP Item tidak ada di lokasi ini!\n(Pesanan VIP di TO_DO harus didahulukan! Gue  bucin!)\n\n");
+            }
         }else{
-            printf("Pesanan berupa VIP Item tidak ada di lokasi ini!\n(Pesanan VIP di TO_DO harus didahulukan! Gue bucin!)\n\n");
+            printf("Tas telah penuh\n\n");
         }
     } else {
         if(gameState.gameMap._locationMatrix.contents[gameState.currentLocation.coordinate.x][gameState.currentLocation.coordinate.y].isPickUpPlace){
-            int countItem = countItemInLocation(gameState.todoList);
-            printf("%d", countItem);
-            int i;
-            for(i = 0; i<countItem; i++){//Loop Untuk Mengambil Beberapa Item pada Pick Up Place
-                int idx = idxItemInTodo(gameState.todoList);
-                if(!isStackFull(gameState.bag)){
-                    if(isNormalItem(getItem(gameState.todoList, idx))){
-                        insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
-                        push(&gameState.bag, getItem(gameState.todoList, idx));
-                        printf("Pesanan berupa Normal Item berhasil diambil!\n");
-                        printf("Tujuan Pesanan : %c\n\n", getItem(gameState.todoList, idx).dropOffLocation.symbol);
-                        Item temp;
-                        deleteItemAt(&gameState.todoList, idx, &temp); //Menghapus item yang di Pick_UP dari To Do
-                    }else if (isHeavyItem(getItem(gameState.todoList, idx))){
-                        insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
-                        push(&gameState.bag, getItem(gameState.todoList, idx));
-                        printf("Pesanan berupa Heavy Item berhasil diambil!\n");
-                        printf("Tujuan Pesanan : %c\n\n", getItem(gameState.todoList, idx).dropOffLocation.symbol);
-                        Item temp;
-                        deleteItemAt(&gameState.todoList, idx, &temp);
-                        gameState.abs.IsHeavyItemOn = true;
-                        gameState.abs.HeavyItemStack++;
-                        if (gameState.abs.SpeedBoost){
-                            gameState.abs.SpeedBoost = false;
-                            gameState.abs.SpeedBoostCount = 0;
-                            gameState.abs.SpeedBoostStack = 0;
-                            printf("Speed Boost yang didapatkan dimatikan!");
-                        }
-                    }else if(isPerishableItem(getItem(gameState.todoList, idx))){
-                        insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
-                        push(&gameState.bag, getItem(gameState.todoList, idx));
-                        printf("Pesanan berupa Perishable Item berhasil diambil!\n");
-                        printf("Tujuan Pesanan : %c\n\n", getItem(gameState.todoList, idx).dropOffLocation.symbol);
-                        Item temp;
-                        deleteItemAt(&gameState.todoList, idx, &temp);
+            int idx = idxItemInTodo(gameState.todoList);
+            if(!isStackFull(gameState.bag)){
+                if(isNormalItem(getItem(gameState.todoList, idx))){
+                    insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
+                    push(&gameState.bag, getItem(gameState.todoList, idx));
+                    printf("Pesanan berupa Normal Item berhasil diambil!\n");
+                    printf("Tujuan Pesanan : %c\n\n", getItem(gameState.todoList, idx).dropOffLocation.symbol);
+                    Item temp;
+                    deleteItemAt(&gameState.todoList, idx, &temp); //Menghapus item yang di Pick_UP dari To Do
+                }else if (isHeavyItem(getItem(gameState.todoList, idx))){
+                    insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
+                    push(&gameState.bag, getItem(gameState.todoList, idx));
+                    printf("Pesanan berupa Heavy Item berhasil diambil!\n");
+                    printf("Tujuan Pesanan : %c\n\n", getItem(gameState.todoList, idx).dropOffLocation.symbol);
+                    Item temp;
+                    deleteItemAt(&gameState.todoList, idx, &temp);
+                    gameState.abs.IsHeavyItemOn = true;
+                    gameState.abs.HeavyItemStack++;
+                    if (gameState.abs.SpeedBoost){
+                        gameState.abs.SpeedBoost = false;
+                        gameState.abs.SpeedBoostCount = 0;
+                        gameState.abs.SpeedBoostStack = 0;
+                        printf("Speed Boost yang didapatkan dimatikan!");
                     }
-                }else{
-                    printf("Tas telah penuh\n\n");
+                }else if(isPerishableItem(getItem(gameState.todoList, idx))){
+                    insertItem(&gameState.inProgressList, getItem(gameState.todoList, idx));
+                    push(&gameState.bag, getItem(gameState.todoList, idx));
+                    printf("Pesanan berupa Perishable Item berhasil diambil!\n");
+                    printf("Tujuan Pesanan : %c\n\n", getItem(gameState.todoList, idx).dropOffLocation.symbol);
+                    Item temp;
+                    deleteItemAt(&gameState.todoList, idx, &temp);
                 }
+            }else{
+                printf("Tas telah penuh\n\n");
             }
         }else{
             printf("Tidak ada pesanan yang dapat diambil\n\n");
